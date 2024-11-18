08136e9e656c5be05f03358b348c4060a56c2d4c
Joanerson Costa <joanersonc31@gmail.com>
2024-11-18 12:44:43 -0300

    inicia projeto

---
+++ src/test/java/com/github/joanersoncosta/servico_cliente_api/ServicoClienteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.joanersoncosta.servico_cliente_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoClienteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
